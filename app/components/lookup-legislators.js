e5218a75f4b1d4bbcb0ddea6f9794635256c918a
Alex Jacks and Joel Delight <joel@>
2016-08-23 09:34:39 -0700

    refactor the legislator lookup into one route.

---
+++ app/components/lookup-legislators.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  legislators: [],
+  displayResults: false,
+  actions: {
+    zipLookup() {
+      var hoc = this;
+      var params = {
+        zip: this.get('zip')
+      };
+      var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=b80d59393f384908b0b6fd75f653e624&zip=' + params.zip;
+      return Ember.$.getJSON(url).then(function(responseJSON) {
+        hoc.set('legislators', responseJSON.results);
+        hoc.set('displayResults', true);
+      });
+    }
+  }
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   actions: {
-    zipLookup(params) {
-      this.transitionTo('results', params.zip);
-    }
+    
   }
 });
