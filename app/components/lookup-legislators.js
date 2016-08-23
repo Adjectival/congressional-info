d336af2dc17cd11b6949d0902ad528d9693e40c5
Alex Jacks and Joel Delight <joel@>
2016-08-23 10:04:59 -0700

    adds bioguide reference to zip lookup; result names now link to individual pages.

--- app/components/lookup-legislators.js
+++ app/components/lookup-legislators.js
@@ -11,6 +11,7 @@ export default Ember.Component.extend({
       };
       var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=b80d59393f384908b0b6fd75f653e624&zip=' + params.zip;
       return Ember.$.getJSON(url).then(function(responseJSON) {
+        console.log(responseJSON.results);
         hoc.set('legislators', responseJSON.results);
         hoc.set('displayResults', true);
       });
--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-
+  this.route('congressperson', {path: '/congressperson/:bioguide'});
 });
 
 export default Router;
